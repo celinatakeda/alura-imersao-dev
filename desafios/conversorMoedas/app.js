ce2cee1b16296aebfa34add910c06dd31399e0ca
Celina Takeda <takedacarina@gmail.com>
2022-03-14 08:16:16 -0300

    commit

--- desafios/conversorMoedas/app.js
+++ desafios/conversorMoedas/app.js
@@ -13,7 +13,7 @@ function Converter() {
     if(opcao != null) {
       var realEmDolar = valor * 5;
       var elementoValorConvertido = document.getElementById("valorConvertidoReal");
-      elementoValorConvertido.innerHTML = "R$ " + realEmDolar;
+      elementoValorConvertido.innerHTML = "R$ " + realEmDolar.toFixed(2);
     }
   }
 
@@ -21,15 +21,15 @@ function Converter() {
     if(opcao != null) {
       var DolarEmReal = valor / 5;
       var elementoValorConvertido = document.getElementById("valorConvertidoDolar");
-      elementoValorConvertido.innerHTML = "U$ " + DolarEmReal;
+      elementoValorConvertido.innerHTML = "U$ " + DolarEmReal.toFixed(2);
     }
   }
 
   function ConverterRealBitcoin(valor, opcao) {
     if(opcao != null) {
-      var realEmBitcoin = valor * 2;
+      var realEmBitcoin = valor * 0.0000050;
       var elementoValorConvertido = document.getElementById("valorConvertidoBitcoin");
-      elementoValorConvertido.innerHTML = "BTC " + realEmBitcoin;
+      elementoValorConvertido.innerHTML = "BTC " + realEmBitcoin.toFixed(2);
     }
   }
 
