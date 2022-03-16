dfa52eedb914d9efb320b75e034ec10f383396e9
Celina Takeda <takedacarina@gmail.com>
2022-03-16 17:20:30 -0300

    commit

--- aula08/script.js
+++ aula08/script.js
@@ -11,7 +11,7 @@ var cartaPaulo = {
   };
   
   var cartaRafa = {
-    nome: "Bulbasaur",
+    nome: "Bulbasauro",
     imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
     atributos: {
       ataque: 7,
@@ -32,7 +32,7 @@ var cartaPaulo = {
   };
 
   var cartaPiplup = {
-    nome: "Piplup",
+    nome: "Piplup Tail",
     imagem:
         "https://www.pngfind.com/pngs/m/441-4413614_personagem-pokemon-png-piplup-tail-transparent-png.png",
       //"http://pm1.narvii.com/6399/96fdb9d4fe6a9e72b9bc60ad418e3c43795e53b4_00.jpg",
@@ -43,74 +43,41 @@ var cartaPaulo = {
     }
   };
 
-  var cartaPachirisu = {
-    nome: "Pachirisu",
+  var cartaMeotwo = {
+    nome: "Meotwo",
     imagem:
-        "https://i.pinimg.com/originals/17/fa/d8/17fad813cda380f2d310d24b18cbfd34.png",      
+        "https://i.pinimg.com/originals/17/fa/d8/17fad813cda380f2d310d24b18cbfd34.png",
+      //"http://pm1.narvii.com/6399/96fdb9d4fe6a9e72b9bc60ad418e3c43795e53b4_00.jpg",
     atributos: {
       ataque: 8,
       defesa: 7,
       magia: 9
     }
   };
-
-  var cartaVaporeon = {
-    nome: "Eeveelution Vaporeon",
-    imagem: 
-        "https://1.bp.blogspot.com/-6_1ISesmTB4/V6Co1X0kPfI/AAAAAAAAAYk/xO28MIPcc_A9hy0eCV2PqTciopZqACrOgCLcB/s1600/134Vaporeon_BW_anime.png",
-    atributos: {
-        ataque: 8,
-        defesa: 7,
-        magia: 9
-     }
-    };
-
-    var cartaVupix = {
-        nome: "Vupix",
-        imagem: "https://vignette.wikia.nocookie.net/pokexpertodex/images/7/7f/Vulpix_usando_lanzallamas.PNG/revision/latest?cb=20121116114851&path-prefix=es",
-        atributos: {
-            ataque: 8,
-            defesa: 7,
-            magia: 9
-        }
-    };
-
-    var cartaGyarados = {
-        nome: "Gyarados",
-        imagem: "https://th.bing.com/th/id/R.9c798b57e1e597b8de4d3f70f897fdab?rik=w4%2bU45mF3iEmpg&riu=http%3a%2f%2f2.bp.blogspot.com%2f-fSPLtwNKTQw%2fV9hh9xmkQhI%2fAAAAAAAABj4%2faM9pQxrpnnowUnRuMeAB8OrJuE54D6RyACK4B%2fs1600%2fe464eb10-35ab-0134-066a-062f3a35be5f.png&ehk=ck97Ac3rYm3nyZ%2fyvSJjYgEmg4KnXhI03pzQTJtmL3Y%3d&risl=&pid=ImgRaw&r=0",
-        atributos: {
-            ataque: 8,
-            defesa: 7,
-            magia: 7
-        }
-    };
-
-    var cartaSeya = {
-        nome: "Seya",
-        imagem: "https://i.pinimg.com/originals/16/0c/ff/160cff418f023094911f9dc42d757065.png",
-        atributos: {
-            ataque: 10,
-            defesa: 8,
-            magia: 6
-        }
-    };
-
   
-  var cartaMaquina;
-  var cartaJogador;
-  var cartas = [cartaPaulo, cartaRafa, cartaGui, cartaPiplup, cartaPachirisu, cartaVaporeon, cartaVupix, cartaGyarados, cartaSeya ];
+  var cartaMaquina = [cartaPaulo, cartaRafa, cartaGui, cartaPiplup, cartaMeotwo];
+  var cartaJogador = [cartaPaulo, cartaRafa, cartaGui, cartaPiplup, cartaMeotwo];
+  var indiceMaquina = 0;
+  var indiceJogador = 0;
+  //var cartas = [cartaPaulo, cartaRafa, cartaGui, cartaPiplup, cartaMeotwo];
   // 0          1           2
+  //console.log(cartaMaquina.indexOf(cartaPiplup)) 
+  console.log(cartaMaquina[indiceMaquina]) 
   
   function sortearCarta() {
-    var numeroCartaMaquina = parseInt(Math.random() * 9);
-    cartaMaquina = cartas[numeroCartaMaquina];
+    var numeroCartaMaquina = parseInt(Math.random() * 5);
+    cartaMaquina = cartaMaquina[numeroCartaMaquina];
+    indiceMaquina = numeroCartaMaquina
+    
+    //console.log(indiceMaquina);
   
-    var numeroCartaJogador = parseInt(Math.random() * 9);
+    var numeroCartaJogador = parseInt(Math.random() * 5);
     while (numeroCartaJogador == numeroCartaMaquina) {
-      numeroCartaJogador = parseInt(Math.random() * 9);
+      numeroCartaJogador = parseInt(Math.random() * 5);
     }
-    cartaJogador = cartas[numeroCartaJogador];
-    console.log(cartaJogador);
+    cartaJogador = cartaJogador[numeroCartaJogador];
+    indiceJogador = numeroCartaJogador;
+    //console.log(indiceJogador);
   
     document.getElementById("btnSortear").disabled = true;
     document.getElementById("btnJogar").disabled = false;
@@ -136,8 +103,13 @@ var cartaPaulo = {
         return
     }
   
-    if (cartaJogador.atributos[atributoSelecionado] > cartaMaquina.atributos[atributoSelecionado]
-    ) {
+    if (cartaJogador.atributos[atributoSelecionado] > cartaMaquina.atributos[atributoSelecionado])    
+     {  
+        console.log(indiceMaquina)
+        
+        console.log(cartaMaquina)
+         
+         receberCarta();            
       htmlResultado = "<p class='resultado-final'>Venceu!</p>";
     } else if (
       cartaJogador.atributos[atributoSelecionado] <
@@ -151,6 +123,22 @@ var cartaPaulo = {
   
     document.getElementById("btnJogar").disabled = true;
     exibirCartaMaquina();
+  }
+
+  function receberCarta() {
+      console.log(indiceMaquina)
+      console.log(indiceJogador)
+      //var indexCartaMaquina = cartaMaquina.indexOf()
+      //console.log(indexCartaMaquina)
+      console.log(cartaMaquina)
+       //cartaMaquina[indiceMaquina]
+      console.log(cartaMaquina[indiceMaquina])
+      //console.log(cartaJogador[indiceJogador])
+       cartaJogador.push(cartaMaquina[indiceMaquina]);
+       cartaMaquina.splice(indiceMaquina, 1)
+       console.log(cartaMaquina)
+       console.log(cartaJogador)
+
   }
   
   function exibirCartaJogador() {
