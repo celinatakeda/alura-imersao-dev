bed4bbe0392c92f2188661f0f43c0619b76d202b
Celina Takeda <82118386+celinatakeda@users.noreply.github.com>
2022-09-05 11:21:56 -0300

    commit

---
+++ edicao5/conversorTemperatura/script.js
@@ -0,0 +1,35 @@
+function ConverterFarenheit() {
+  var valorElemento = document.getElementById("valor");
+  var valor = valorElemento.value;
+
+  if (valor !== "") {
+    var valorCelsius = parseFloat(valor);
+    var valorFarenheit = (valorCelsius * 1.8) + 32;
+    var elementoValorConvertido = document.getElementById("valorConvertido");
+
+    var valorConvertido = "O resultado é " + valorFarenheit + "F";
+    elementoValorConvertido.innerHTML = valorConvertido;
+  }else {
+    var valorIndefinido = "Digite a temperatura"
+    var elementoValorConvertido = document.getElementById("valorConvertido");
+    elementoValorConvertido.innerHTML = valorIndefinido;
+  }
+}
+
+function ConverterCelsius() {
+  var valorElemento = document.getElementById("valor");
+  var valor = valorElemento.value;
+
+  if (valor !== "") {
+    var valorFarenheit = parseFloat(valor);
+    var valorCelsius = (valorFarenheit - 32) / 1.8;
+    var elementoValorConvertido = document.getElementById("valorConvertido");
+
+    var valorConvertido = "O resultado é " + valorCelsius + "C";
+    elementoValorConvertido.innerHTML = valorConvertido;
+  }else {
+    var valorIndefinido = "Digite a temperatura"
+    var elementoValorConvertido = document.getElementById("valorConvertido");
+    elementoValorConvertido.innerHTML = valorIndefinido;
+  }
+}
