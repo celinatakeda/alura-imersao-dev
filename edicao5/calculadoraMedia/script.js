bc7eab9a7183f56df3f579ce366cff9293ae75b5
Celina Takeda <82118386+celinatakeda@users.noreply.github.com>
2022-09-05 10:27:42 -0300

    commit

---
+++ edicao5/calculadoraMedia/script.js
@@ -0,0 +1,37 @@
+function CalcularMedia() {
+  var elementoNotaUm = document.getElementById("nota1");
+  var elementoNotaDois = document.getElementById("nota2");
+  var elementoNotaTres = document.getElementById("nota3");
+  var elementoNotaQuatro = document.getElementById("nota4");
+
+  var nota1 = elementoNotaUm.value;
+  var nota2 = elementoNotaDois.value;
+  var nota3 = elementoNotaTres.value;
+  var nota4 = elementoNotaQuatro.value;
+
+  if (nota1 !== "" && nota2 !== "" && nota3 !== "" && nota4 !== "") {
+    var nota1Numerico = parseFloat(nota1);
+    var nota2Numerico = parseFloat(nota2);
+    var nota3Numerico = parseFloat(nota3);
+    var nota4Numerico = parseFloat(nota4);
+
+    var media =
+      (nota1Numerico + nota2Numerico + nota3Numerico + nota4Numerico) / 4;
+    console.log(media);
+
+    var elementoResultadoMedia = document.getElementById("resultadoMedia");
+
+    if (media >= 7) {
+      var mediaFinal =
+        "Parabéns, você foi Aprovado! Sua média foi: " + media.toFixed(2);
+      elementoResultadoMedia.innerHTML = mediaFinal;
+    } else {
+      var mediaFinal =
+        "Infelizmente, você foi Reprovado! Sua média foi: " + media.toFixed(1);
+      elementoResultadoMedia.innerHTML = mediaFinal;
+    }
+  } else {
+    var elementoResultadoMedia = document.getElementById("resultadoMedia");
+    elementoResultadoMedia.innerHTML = "Digite todas as notas";
+  }
+}
