540a30f6fe6a8446d3743b6ac9822e2e031f9ebc
Celina Takeda <82118386+celinatakeda@users.noreply.github.com>
2022-09-06 11:50:17 -0300

    commit

--- edicao5/conversorMoedas/script.js
+++ edicao5/conversorMoedas/script.js
@@ -5,12 +5,36 @@ function ConverterReal() {
     var valorEmDolarNumerico = parseFloat(valor);
 
     var valorEmReal = valorEmDolarNumerico * 5;
-    var valorEmBitcoin = (valorEmDolarNumerico * 102.905, 70) / 1000000;
+    var valorEmBitcoin = (valorEmDolarNumerico * 102.905,70) / 1000000;
 
     var elementoValorConvertido = document.getElementById("valorConvertido");
     var elementoValorBitcoin = document.getElementById("valorEmBitcoin");
 
-    var valorConvertido = "O resultado em Real é R$ " + valorEmReal;
+    var valorConvertido = "O valor em Real é R$ " + valorEmReal.toFixed(2);
+    elementoValorConvertido.innerHTML = valorConvertido;
+
+    var valorBitcoin = "O valor em Bitcoin é Ƀ " + valorEmBitcoin;
+    elementoValorBitcoin.innerHTML = valorBitcoin;
+  } else {
+    var valorIndefinido = "Digite o valor!";
+    var elementoValorConvertido = document.getElementById("valorConvertido");
+    elementoValorConvertido.innerHTML = valorIndefinido;
+  }
+}
+
+function ConverterEuro() {
+  var valorElemento = document.getElementById("valor");
+  var valor = valorElemento.value;
+  if (valor !== "") {
+    var valorEmRealNumerico = parseFloat(valor);
+
+    var valorEmEuro = valorEmRealNumerico * 5;
+    var valorEmBitcoin = (valorEmRealNumerico * 102.905,70) / 1000000;
+
+    var elementoValorConvertido = document.getElementById("valorConvertido");
+    var elementoValorBitcoin = document.getElementById("valorEmBitcoin");
+
+    var valorConvertido = "O valor em Euro é € " + valorEmEuro.toFixed(2);
     elementoValorConvertido.innerHTML = valorConvertido;
 
     var valorBitcoin = "O valor em Bitcoin é Ƀ " + valorEmBitcoin;
@@ -35,7 +59,7 @@ function ConverterDolar() {
     var elementoValorConvertido = document.getElementById("valorConvertido");
     var elementoValorBitcoin = document.getElementById("valorEmBitcoin");
 
-    var valorConvertido = "O resultado em Dolar é U$ " + valorEmDolar;
+    var valorConvertido = "O valor em Dólar é U$ " + valorEmDolar.toFixed(2);
     elementoValorConvertido.innerHTML = valorConvertido;
 
     var valorBitcoin = "O valor em Bitcoin é Ƀ " + valorEmBitcoin;
