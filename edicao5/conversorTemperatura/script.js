cbf9d27727da2dc43dd55f2cef0eb78350243737
Celina Takeda <82118386+celinatakeda@users.noreply.github.com>
2022-09-06 10:12:35 -0300

    commit

--- edicao5/conversorTemperatura/script.js
+++ edicao5/conversorTemperatura/script.js
@@ -7,7 +7,26 @@ function ConverterFarenheit() {
     var valorFarenheit = (valorCelsius * 1.8) + 32;
     var elementoValorConvertido = document.getElementById("valorConvertido");
 
-    var valorConvertido = "O resultado é " + valorFarenheit + "F";
+    var valorConvertido = "O resultado é " + valorFarenheit.toFixed(0) + "F";
+    elementoValorConvertido.innerHTML = valorConvertido;
+  }else {
+    var valorIndefinido = "Digite a temperatura"
+    var elementoValorConvertido = document.getElementById("valorConvertido");
+    elementoValorConvertido.innerHTML = valorIndefinido;
+  }
+}
+
+function ConverterKelvin() {
+  var valorElemento = document.getElementById("valor");
+  var valor = valorElemento.value;
+
+  if (valor !== "") {
+    var valorCelsius = parseFloat(valor);
+
+    var valorKelvis = valorCelsius + 273.15;
+    var elementoValorConvertido = document.getElementById("valorConvertido");
+
+    var valorConvertido = "O resultado é " + valorKelvis.toFixed(0) + "K";
     elementoValorConvertido.innerHTML = valorConvertido;
   }else {
     var valorIndefinido = "Digite a temperatura"
@@ -25,7 +44,7 @@ function ConverterCelsius() {
     var valorCelsius = (valorFarenheit - 32) / 1.8;
     var elementoValorConvertido = document.getElementById("valorConvertido");
 
-    var valorConvertido = "O resultado é " + valorCelsius.toFixed(2) + "C";
+    var valorConvertido = "O resultado é " + valorCelsius.toFixed(0) + "C";
     elementoValorConvertido.innerHTML = valorConvertido;
   }else {
     var valorIndefinido = "Digite a temperatura"
