b0f3734bca33a09bf1173a0190c303d912597a82
Celina Takeda <82118386+celinatakeda@users.noreply.github.com>
2022-11-07 09:24:23 -0300

    commit

---
+++ react/aluratube/.next/server/pages/index.js
@@ -0,0 +1,46 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/index";
+exports.ids = ["pages/index"];
+exports.modules = {
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n\nfunction HomePage() {\n    const estiloDaHomePage = {\n        backgroundColor: \"red\"\n    };\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"div\", {\n        style: estiloDaHomePage,\n        children: \"Bem vindo ao AluraTube!\"\n    }, void 0, false, {\n        fileName: \"C:\\\\curso\\\\repositories\\\\alura-imersao-dev\\\\react\\\\aluratube\\\\pages\\\\index.js\",\n        lineNumber: 4,\n        columnNumber: 5\n    }, this);\n}\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (HomePage);\nfunction Menu() {\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"div\", {\n        children: \"Menu\"\n    }, void 0, false, {\n        fileName: \"C:\\\\curso\\\\repositories\\\\alura-imersao-dev\\\\react\\\\aluratube\\\\pages\\\\index.js\",\n        lineNumber: 12,\n        columnNumber: 5\n    }, this);\n}\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9pbmRleC5qcy5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7QUFBQTtBQUFBLFNBQVNBLFdBQVc7SUFDbEIsTUFBTUMsbUJBQW1CO1FBQUNDLGlCQUFpQjtJQUFLO0lBQ2hELHFCQUNFLDhEQUFDQztRQUFJQyxPQUFRSDtrQkFBbUI7Ozs7OztBQUVwQztBQUVBLGlFQUFlRCxRQUFRQSxFQUFBO0FBRXZCLFNBQVNLLE9BQU87SUFDZCxxQkFDRSw4REFBQ0Y7a0JBQUk7Ozs7OztBQUlUIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vLy4vcGFnZXMvaW5kZXguanM/YmVlNyJdLCJzb3VyY2VzQ29udGVudCI6WyJmdW5jdGlvbiBIb21lUGFnZSgpIHtcclxuICBjb25zdCBlc3RpbG9EYUhvbWVQYWdlID0ge2JhY2tncm91bmRDb2xvcjogJ3JlZCd9O1xyXG4gIHJldHVybiAoXHJcbiAgICA8ZGl2IHN0eWxlPXsgZXN0aWxvRGFIb21lUGFnZSB9PkJlbSB2aW5kbyBhbyBBbHVyYVR1YmUhPC9kaXY+XHJcbiAgKTtcclxufVxyXG5cclxuZXhwb3J0IGRlZmF1bHQgSG9tZVBhZ2VcclxuXHJcbmZ1bmN0aW9uIE1lbnUoKSB7XHJcbiAgcmV0dXJuIChcclxuICAgIDxkaXY+XHJcbiAgICAgIE1lbnVcclxuICAgIDwvZGl2PlxyXG4gIClcclxufSJdLCJuYW1lcyI6WyJIb21lUGFnZSIsImVzdGlsb0RhSG9tZVBhZ2UiLCJiYWNrZ3JvdW5kQ29sb3IiLCJkaXYiLCJzdHlsZSIsIk1lbnUiXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./pages/index.js\n");
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
+
+module.exports = require("react/jsx-dev-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./pages/index.js"));
+module.exports = __webpack_exports__;
+
+})();
---
+++ react/aluratube/pages/index.js
@@ -0,0 +1,32 @@
+function HomePage() {
+  const estiloDaHomePage = {backgroundColor: 'red'};
+  return (
+    <div style={ estiloDaHomePage }>Bem vindo ao AluraTube!</div>
+  );
+}
+
+export default HomePage
+
+function Menu() {
+  return (
+    <div>
+      Menu
+    </div>
+  )
+}
+
+function Menu() {
+  return (
+    <div>
+      Menu
+    </div>
+  )
+}
+
+function Menu() {
+  return (
+    <div>
+      Menu
+    </div>
+  )
+}
