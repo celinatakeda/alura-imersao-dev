c2913aa6b367e92e06558fd66aab3d29e8a7d34c
Celina Takeda <82118386+celinatakeda@users.noreply.github.com>
2022-11-10 09:47:09 -0300

    commit

--- react/aluratube/pages/_app.js
+++ react/aluratube/pages/_app.js
@@ -2,6 +2,7 @@ import React from "react";
 import { ThemeProvider } from "styled-components";
 import { CSSReset } from "../src/components/CSSReset";
 import ColorModeProvider, { ColorModeContext } from "../src/components/Menu/components/ColorMode";
+import RegisterVideo from "../src/components/RegisterVideo";
 
 const theme = {
     light: {
@@ -32,21 +33,22 @@ function ProviderWrapper(props) {
     )
 }
 
-function MyApp({ Component, pageProps }) {
+function Root({ Component, pageProps }) {
     const contexto = React.useContext(ColorModeContext);
     console.log(contexto.mode);
     return (
         <ThemeProvider theme={theme[contexto.mode]}>
             <CSSReset />
             <Component {...pageProps} />
+            <RegisterVideo />
         </ThemeProvider>
     )
 }
 
 export default function _App(props) {
     return (
-        <ProviderWrapper>
-            <MyApp {...props} />
+        <ProviderWrapper>           
+            <Root {...props} />
         </ProviderWrapper>
     )
 };
