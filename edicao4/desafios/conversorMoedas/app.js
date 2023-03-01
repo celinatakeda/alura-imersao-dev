3788e8fc5e8e04ea3b08278f9f21c181973e8bbe
Celina Takeda <82118386+celinatakeda@users.noreply.github.com>
2023-03-01 09:55:47 -0300

    commit

---
+++ edicao4/desafios/conversorMoedas/app.js
@@ -0,0 +1,34 @@
+function Converter() {
+  var elementoValor = document.getElementById("valor").value;
+  var elementoOpcao1 = document.querySelector("input[name=dolarReal]:checked");
+  var elementoOpcao2 = document.querySelector("input[name=realDolar]:checked");
+  var elementoOpcao3 = document.querySelector("input[name=realBitcoin]:checked");
+
+  ConverterRealDolar(elementoValor, elementoOpcao1);
+  ConverterDolarReal(elementoValor, elementoOpcao2);
+  ConverterRealBitcoin(elementoValor, elementoOpcao3);   
+}
+
+  function ConverterRealDolar(valor, opcao) {
+  if(opcao != null) {
+    var realEmDolar = valor * 5;
+    var elementoValorConvertido = document.getElementById("valorConvertidoReal");
+    elementoValorConvertido.innerHTML = "R$ " + realEmDolar.toFixed(2);
+  }
+}
+
+function ConverterDolarReal(valor, opcao) {
+  if(opcao != null) {
+    var DolarEmReal = valor / 5;
+    var elementoValorConvertido = document.getElementById("valorConvertidoDolar");
+    elementoValorConvertido.innerHTML = "U$ " + DolarEmReal.toFixed(2);
+  }
+}
+
+function ConverterRealBitcoin(valor, opcao) {
+  if(opcao != null) {
+    var realEmBitcoin = valor * 0.0000050;
+    var elementoValorConvertido = document.getElementById("valorConvertidoBitcoin");
+    elementoValorConvertido.innerHTML = "BTC " + realEmBitcoin.toFixed(2);
+  }
+}
